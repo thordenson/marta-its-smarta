539a8e066bab300ae5144391127a8f3cd31abaf9
Thor Denson <tjdenson@eckerd.edu>
2018-07-23 10:57:27 -0400

    adds componentDidiMount function

--- src/MartaDashboard.js
+++ src/MartaDashboard.js
@@ -1,19 +1,41 @@
 import React from 'react';
 
+const MARTA_URL = 'https://my-little-cors-proxy.herokuapp.com/http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=2c514350-0c26-47dd-b872-7936af81c8e1';
 
-fetch('https://my-little-cors-proxy.herokuapp.com/http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=2c514350-0c26-47dd-b872-7936af81c8e1', {
-    method: 'get'
-}).then(function(response) {
-    return response.json()
-}).then(function(jsonData) {
-    console.log(jsonData);
-}).catch(function(err) {
-    // Error :(
-});
 
 class MartaDashboard extends React.Component {
+    constructor(props) {
+        super(props);
 
-    
+        this.state = {
+            data: []
+        };
+    }
+
+
+    componentDidiMount() {
+        fetch(MARTA_URL, {
+            method: 'get'
+            }).then(function(response) {
+                return response.json()
+            }).then(function(jsonData) {
+                console.log(jsonData);
+                this.setState({
+                    data: jsonData
+                });
+            }).catch(function(err) {
+                // Error :(
+            });
+    }
+
+
+    render() {
+        return (
+            <div> 
+                Marta! Why you take all the time??
+            </div>
+        );
+    }
 }
 
 
